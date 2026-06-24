d524d12f15fe766f9c4bb5fd17184ee2e8ce89c7
Akshithreddy-07 <gangidipullareddy@gmail.com>
2026-06-24 12:19:56 +0530

    support

--- src/Home.jsx
+++ src/Home.jsx
@@ -54,7 +54,7 @@ function Home() {
 
         <div className="info-card">
           <img
-            src="https://images.unsplash.com/photo-1517841905240-472988babdf9?w=600"
+            src="https://th.bing.com/th/id/OIP.RkoPeLTYNyFS5LVVvOKLkwHaGK?w=217&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3"
             alt="Support"
           />
           <h3>24/7 Support</h3>
